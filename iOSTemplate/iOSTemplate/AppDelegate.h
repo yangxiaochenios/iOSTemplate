5d57cd3a5df2fa20a3bc32ca7feb34d2d568aa9e
tm <tm@tmdeimac.local>
2016-03-22 18:24:02 +0800

    初始化

---
+++ iOSTemplate/iOSTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTemplate
+//
+//  Created by tm on 16/3/22.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
